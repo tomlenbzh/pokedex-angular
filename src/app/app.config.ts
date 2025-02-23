5d4a5ac2ac2c83d0afb93a6f7352effb9fc4d3e2
tomlenbzh <tomlenbzh@yggdrasil.lan>
2025-02-23 17:10:38 +0100

    feat(setup): enable hydration options

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,21 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+  withHttpTransferCacheOptions,
+  withIncrementalHydration,
+} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(
+      withEventReplay(),
+      withIncrementalHydration(),
+      withHttpTransferCacheOptions({ includePostRequests: false }),
+    ),
   ],
 };
